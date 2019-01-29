f4984744cb3d7eeec318d2d5e153b2a8881371a6
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-30 00:04:25 +0100

    applyMiddleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
 
 export default (initialState) => {
     return createStore(
         rootReducer,
-        initialState
+        initialState,
+        applyMiddleware(reduxImmutableStateInvariant())
     )
 }
