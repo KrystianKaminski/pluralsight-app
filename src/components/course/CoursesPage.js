33cb6125a143bb53233f9b51bf7f0c302751ac46
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-30 00:43:27 +0100

    bindActionCreators

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import * as courseActions from '../../actions/courseActions'
 
 class CoursesPage extends React.Component {
@@ -26,7 +27,7 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        this.props.createCourse(this.state.course)
+        this.props.actions.createCourse(this.state.course)
     }
 
     courseRow(course, index) {
@@ -57,7 +58,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -68,7 +69,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        createCourse: course => dispatch(courseActions.createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch)
     }
 }
 
