c3db55b19b966398d5ff1d1b1853095b8b2a7d04
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-30 18:46:47 +0100

    Created load courses thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,21 @@
 import * as types from './actionTypes'
+import courseApi from '../api/mockCourseApi'
 
-export const createCourse = course => {
+export const loadCoursesSuccess = courses => {
     return {
-        type: types.CREATE_COURSE,
-        course
+        type: types.LOAD_COURSES_SUCCESS,
+        courses
+    }
+}
+
+export const loadCourses = () => {
+    return (dispatch) => {
+        return courseApi.getAllCourses()
+            .then(courses => {
+                dispatch(loadCoursesSuccess(courses))
+            })
+            .catch(error => {
+                throw (error)
+            })
     }
 }
