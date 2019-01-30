dfef5ed05a79026ea069f827a267929c1bdc77d4
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-30 20:05:10 +0100

    ManageCoursePage component nearly completed

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,28 +2,47 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as courseActions from '../../actions/courseActions'
+import CourseForm from './CourseForm'
 
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context)
+
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        }
     }
+
 
     render() {
         return (
-            <div>
-                <h1>Manage Course</h1>
-            </div>
+            <CourseForm
+                allAuthors={[]}
+                course={this.state.course}
+                errors={this.state.errors}
+            />
         )
     }
 }
 
 ManageCoursePage.propTypes = {
-    //
+    course: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return state
+    let course = {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: '',
+    }
+    return {
+        course: course
+    }
 }
 
 const mapDispatchToProps = dispatch => {
